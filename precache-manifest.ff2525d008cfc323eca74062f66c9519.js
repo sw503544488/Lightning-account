20be105364e86f05933cf1be13dd0c9cae99ad09
sw503544488 <9531592+sw503544488@user.noreply.gitee.com>
2021-09-07 21:36:00 +0800

    deploy

---
+++ precache-manifest.ff2525d008cfc323eca74062f66c9519.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee986d037b0272a4ad68ec6e259bfd6",
+    "url": "/Lightning-bookkeeping-website/QRcode.png"
+  },
+  {
+    "revision": "3d40e4a3380064767eee",
+    "url": "/Lightning-bookkeeping-website/css/app.14761403.css"
+  },
+  {
+    "revision": "d166b64198ed0916075394dc92392e75",
+    "url": "/Lightning-bookkeeping-website/favico.icon"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/Lightning-bookkeeping-website/favico1n.ico"
+  },
+  {
+    "revision": "89bdbd01baee2a1bdcca387981a10738",
+    "url": "/Lightning-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "3d40e4a3380064767eee",
+    "url": "/Lightning-bookkeeping-website/js/app.ed83bd6b.js"
+  },
+  {
+    "revision": "099c51f1989f4d40df68",
+    "url": "/Lightning-bookkeeping-website/js/chunk-vendors.6b6b1983.js"
+  },
+  {
+    "revision": "ac29075c0a19b6c69e61ce6ccb871382",
+    "url": "/Lightning-bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Lightning-bookkeeping-website/robots.txt"
+  },
+  {
+    "revision": "c28d68e2da29e8df8b90534e8c441639",
+    "url": "/Lightning-bookkeeping-website/闪电.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Lightning-bookkeeping-website/precache-manifest.ff2525d008cfc323eca74062f66c9519.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "morney1"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
